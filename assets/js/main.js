bcf9dbdb286bed56f759b6429832f0e9d9c31151
Atif Mustaffa <atifmustaffa@gmail.com>
2021-01-22 02:47:11 +0800

    feat: Add web content

---
+++ assets/js/main.js
@@ -0,0 +1,254 @@
+/**
+* Template Name: MyResume - v2.2.0
+* Template URL: https://bootstrapmade.com/free-html-bootstrap-template-my-resume/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+!(function ($) {
+  "use strict";
+
+  // Personal details
+  var yob = 1995
+  var age = new Date().getFullYear() - yob
+  $('#calc-age').text(age);
+
+  // Preloader
+  $(window).on('load', function () {
+    if ($('#preloader').length) {
+      $('#preloader').delay(100).fadeOut('slow', function () {
+        $(this).remove();
+      });
+    }
+  });
+
+  // Hero typed
+  if ($('.typed').length) {
+    var typed_strings = $(".typed").data('typed-items');
+    typed_strings = typed_strings.split(',')
+    new Typed('.typed', {
+      strings: typed_strings,
+      loop: true,
+      typeSpeed: 50,
+      backSpeed: 40,
+      backDelay: 2000
+    });
+  }
+
+  // Smooth scroll for the navigation menu and links with .scrollto classes
+  $(document).on('click', '.nav-menu a, .scrollto', function (e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      if (target.length) {
+        e.preventDefault();
+
+        var scrollto = target.offset().top;
+
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+
+        if ($(this).parents('.nav-menu, .mobile-nav').length) {
+          $('.nav-menu .active, .mobile-nav .active').removeClass('active');
+          $(this).closest('li').addClass('active');
+        }
+
+        if ($('body').hasClass('mobile-nav-active')) {
+          $('body').removeClass('mobile-nav-active');
+          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+        }
+        return false;
+      }
+    }
+  });
+
+  // Activate smooth scroll on page load with hash links in the url
+  $(document).ready(function () {
+    if (window.location.hash) {
+      var initial_nav = window.location.hash;
+      if ($(initial_nav).length) {
+        var scrollto = $(initial_nav).offset().top;
+        $('html, body').animate({
+          scrollTop: scrollto
+        }, 1500, 'easeInOutExpo');
+      }
+    }
+  });
+
+  $(document).on('click', '.mobile-nav-toggle', function (e) {
+    $('body').toggleClass('mobile-nav-active');
+    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+  });
+
+  $(document).click(function (e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+      if ($('body').hasClass('mobile-nav-active')) {
+        $('body').removeClass('mobile-nav-active');
+        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      }
+    }
+  });
+
+  // Navigation active state on scroll
+  var nav_sections = $('section');
+  var main_nav = $('.nav-menu, #mobile-nav');
+
+  $(window).on('scroll', function () {
+    var cur_pos = $(this).scrollTop() + 300;
+
+    nav_sections.each(function () {
+      var top = $(this).offset().top,
+        bottom = top + $(this).outerHeight();
+
+      if (cur_pos >= top && cur_pos <= bottom) {
+        if (cur_pos <= bottom) {
+          main_nav.find('li').removeClass('active');
+        }
+        main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+      }
+      if (cur_pos < 200) {
+        $(".nav-menu ul:first li:first").addClass('active');
+      }
+    });
+  });
+
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.back-to-top').fadeIn('slow');
+      $('.mobile-nav-menu').css('background', '#fff');
+      $('.mobile-nav-menu').css('box-shadow', '2px 2px 2px rgba(0,0,0,0.1)');
+    } else {
+      $('.back-to-top').fadeOut('slow');
+      $('.mobile-nav-menu').css('background', 'none');
+      $('.mobile-nav-menu').css('box-shadow', 'none');
+    }
+  });
+
+  $('.back-to-top').click(function () {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  });
+
+  // jQuery counterUp
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
+  // Skills section
+  $('.skills-content').waypoint(function () {
+    $('.progress .progress-bar').each(function () {
+      $(this).css("width", $(this).attr("aria-valuenow") + '%');
+    });
+  }, {
+    offset: '80%'
+  });
+
+  // Init AOS
+  function aos_init() {
+    AOS.init({
+      duration: 1000,
+      once: true
+    });
+  }
+
+  // Porfolio isotope and filter
+  $(window).on('load', function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+
+    $('#portfolio-flters li').on('click', function () {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
+      aos_init();
+    });
+
+    // Initiate venobox (lightbox feature used in portofilo)
+    $('.venobox').venobox({
+      'share': false,
+      cb_post_open: function (obj) {
+        // ON OPEN FIX INLINE VBOX POST OPEN TO RESET MARGIN
+        var multiple = obj.find('[id^="multiple"]')
+        if (multiple.length) {
+          $('.vbox-content')[0].classList.add('my-5')
+        }
+        // obj.attr('title', `${obj.attr('title')} build with ${desc}`)
+      }
+    });
+
+    // Initiate aos_init() function
+    aos_init();
+
+  });
+
+  // Testimonials carousel (uses the Owl Carousel library)
+  $(".testimonials-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    smartSpeed: 1000,
+    items: 1
+  });
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
+
+  // Portfolio button link navigate
+  var button_links = $('.portfolio-info .portfolio-links').find('button');
+  for (var link of button_links) {
+    $(link).click(function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      window.open($(this).attr('data-open'), '_blank');
+    })
+  }
+
+  // Bootstrap popover enable
+  $('[data-toggle="popover"]').popover({
+    // trigger: 'focus'
+  });
+
+  $(".info .email").on('click', function () {
+    var mail = document.createElement("a");
+    mail.href = "mailto: atifmustaffa@gmail.com";
+    mail.click();
+  });
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener('submit', function (event) {
+      event.preventDefault()
+      event.stopPropagation()
+      form.classList.add('was-validated')
+    }, false)
+  });
+
+  $("#send-message").on('click', function () {
+    // Form validation
+    var form = document.querySelector('.email-form')
+    var formData = new FormData(form)
+    if (form.checkValidity()) {
+      console.log(form.checkValidity())
+      var mail = document.createElement("a");
+      mail.href = `mailto:${formData.get('email')}?subject=${encodeURIComponent(formData.get('subject'))}?body=${encodeURIComponent(formData.get('message'))}`;
+      mail.click();
+    }
+  });
+
+})(jQuery);
